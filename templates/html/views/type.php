9441f19c0c68270cc148bb3694cbf8744b82279a
Carsten Brandt <mail@cebe.cc>
2015-05-13 12:26:42 +0200

    fixed wording for parent interfaces
    extends instead of implements
    
    fixes #3

--- templates/html/views/type.php
+++ templates/html/views/type.php
@@ -55,7 +55,7 @@ $renderer = $this->context;
         <tr><th>Implements</th><td><?= $renderer->renderInterfaces($type->interfaces) ?></td></tr>
     <?php endif; ?>
     <?php if ($type instanceof InterfaceDoc && !empty($type->parentInterfaces)): ?>
-        <tr><th>Implements</th><td><?= $renderer->renderInterfaces($type->parentInterfaces) ?></td></tr>
+        <tr><th>Extends</th><td><?= $renderer->renderInterfaces($type->parentInterfaces) ?></td></tr>
     <?php endif; ?>
     <?php if (!($type instanceof InterfaceDoc) && !empty($type->traits)): ?>
         <tr><th>Uses Traits</th><td><?= $renderer->renderTraits($type->traits) ?></td></tr>
