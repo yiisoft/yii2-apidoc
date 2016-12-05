568156d2eecbd19a1229d40ad4d20102f19fdbcc
Carsten Brandt <mail@cebe.cc>
2016-12-05 02:38:43 +0100

    extracted markdown code highlighting to a trait

--- helpers/ApiMarkdown.php
+++ helpers/ApiMarkdown.php
@@ -8,8 +8,6 @@
 namespace yii\apidoc\helpers;
 
 use cebe\markdown\GithubMarkdown;
-use DomainException;
-use Highlight\Highlighter;
 use yii\apidoc\models\TypeDoc;
 use yii\apidoc\renderers\BaseRenderer;
 use yii\helpers\Html;
@@ -25,6 +23,7 @@ use yii\helpers\Markdown;
 class ApiMarkdown extends GithubMarkdown
 {
     use ApiMarkdownTrait;
+    use MarkdownHighlightTrait;
 
     /**
      * @var BaseRenderer
@@ -83,69 +82,6 @@ class ApiMarkdown extends GithubMarkdown
                 $content = $toc . $content;
         }
         return $content;
-    }
-
-    /**
-     * @var Highlighter
-     */
-    private static $highlighter;
-
-    /**
-     * @inheritdoc
-     */
-    protected function renderCode($block)
-    {
-        if (self::$highlighter === null) {
-            self::$highlighter = new Highlighter();
-            self::$highlighter->setAutodetectLanguages([
-                'apache', 'nginx',
-                'bash', 'dockerfile', 'http',
-                'css', 'less', 'scss',
-                'javascript', 'json', 'markdown',
-                'php', 'sql', 'twig', 'xml',
-            ]);
-        }
-        try {
-            if (isset($block['language'])) {
-                $result = self::$highlighter->highlight($block['language'], $block['content'] . "\n");
-                return "<pre><code class=\"hljs {$result->language} language-{$block['language']}\">{$result->value}</code></pre>\n";
-            } else {
-                $result = self::$highlighter->highlightAuto($block['content'] . "\n");
-                return "<pre><code class=\"hljs {$result->language}\">{$result->value}</code></pre>\n";
-            }
-        } catch (DomainException $e) {
-            echo $e;
-            return parent::renderCode($block);
-        }
-    }
-
-    /**
-     * Highlights code
-     *
-     * @param string $code code to highlight
-     * @param string $language language of the code to highlight
-     * @return string HTML of highlighted code
-     * @deprecated since 2.0.5 this method is not used anymore, highlight.php is used for highlighting
-     */
-    public static function highlight($code, $language)
-    {
-        if ($language !== 'php') {
-            return htmlspecialchars($code, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
-        }
-
-        if (strncmp($code, '<?php', 5) === 0) {
-            $text = @highlight_string(trim($code), true);
-        } else {
-            $text = highlight_string("<?php ".trim($code), true);
-            $text = str_replace('&lt;?php', '', $text);
-            if (($pos = strpos($text, '&nbsp;')) !== false) {
-                $text = substr($text, 0, $pos) . substr($text, $pos + 6);
-            }
-        }
-        // remove <code><span style="color: #000000">\n and </span>tags added by php
-        $text = substr(trim($text), 36, -16);
-
-        return $text;
     }
 
     /**
