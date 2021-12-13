90b0bd890adbd4ce711c0041a95e7a6340fe7514
Alexey Rogachev <arogachev90@gmail.com>
2021-12-13 17:47:45 +0600

    Sync with master changes

--- templates/html/views/methodSourceCode.php
+++ templates/html/views/methodSourceCode.php
@@ -6,19 +6,22 @@ use yii\apidoc\models\MethodDoc;
 /* @var $highlighter \Highlight\Highlighter */
 
 $sourceCode = $method->sourceCode;
-$collapseId = 'collapse' . ucfirst($method->name);
 ?>
 
-<p>
-    <a class="btn btn-link" data-toggle="collapse" href="#<?= $collapseId ?>" role="button" aria-expanded="false"
-       aria-controls="<?= $collapseId ?>">
-        Source code
-    </a>
-</p>
-<div class="collapse" id="<?= $collapseId ?>">
-    <div class="card card-body">
-        <pre>
-            <code class="hljs php language-php"><?= $highlighter->highlight('php', $sourceCode)->value ?></code>
-        </pre>
+<?php if ($sourceCode) {
+    $collapseId = 'collapse' . ucfirst($method->name); ?>
+
+    <p>
+        <a class="btn btn-link" data-toggle="collapse" href="#<?= $collapseId ?>" role="button" aria-expanded="false"
+           aria-controls="<?= $collapseId ?>">
+            Source code
+        </a>
+    </p>
+    <div class="collapse" id="<?= $collapseId ?>">
+        <div class="card card-body">
+            <pre>
+                <code class="hljs php language-php"><?= $highlighter->highlight('php', $sourceCode)->value ?></code>
+            </pre>
+        </div>
     </div>
-</div>
+<?php } ?>
