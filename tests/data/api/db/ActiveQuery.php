6460dc4c98f9cf9c2cb2618c8bd75f3900071718
Максим Спирков <spirkov.2001@mail.ru>
2025-11-10 10:30:19 +0400

    clean tests

--- tests/data/api/db/ActiveQuery.php
+++ tests/data/api/db/ActiveQuery.php
@@ -6,9 +6,6 @@ namespace yiiunit\apidoc\data\api\db;
  * ActiveQuery represents a DB query associated with an Active Record class.
  *
  * @template T of (ActiveRecord|array)
- *
- * @method T|null one($db = null)
- * @method T[] all($db = null)
  * @method ($value is true ? (T is array ? static<T> : static<array<string, mixed>>) : static<T>) asArray($value = true)
  * @method BatchQueryResult<int, T[]> batch($batchSize = 100, $db = null)
  * @method BatchQueryResult<int, T> each($batchSize = 100, $db = null)
