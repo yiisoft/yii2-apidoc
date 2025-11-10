60b5b897ad926f74398f32c41151ff8cb35e77ca
Максим Спирков <spirkov.2001@mail.ru>
2025-11-10 21:29:11 +0400

    remove navigation href

--- tests/commands/ApiControllerTest.php
+++ tests/commands/ApiControllerTest.php
@@ -83,6 +83,7 @@ class ApiControllerTest extends TestCase
             $fileContent = preg_replace('/<script\s+src=".*?"><\/script>/is', '', $fileContent);
             $fileContent = preg_replace('/<link\s+href=".*?" rel="stylesheet">/is', '', $fileContent);
             $fileContent = preg_replace('/\s+id\s*=\s*(["\'])[^"\']*\1/i', '', $fileContent);
+            $fileContent = preg_replace('/\s+href\s*=\s*(["\'])#navigation[^"\']*\1/i', '', $fileContent);
 
             $this->assertMatchesHtmlSnapshot($fileContent);
             $filesCount++;
