860b47ef45c61a73d621219872e5393c331553a0
Максим Спирков <spirkov.2001@mail.ru>
2025-11-23 22:17:40 +0400

    self review

--- tests/data/api/base/Component.php
+++ tests/data/api/base/Component.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
 namespace yiiunit\apidoc\data\api\base;
 
-class Component {}
+class Component
+{
+}
--- tests/data/api/web/AssetManager.php
+++ tests/data/api/web/AssetManager.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
 namespace yiiunit\apidoc\data\api\web;
 
 use yiiunit\apidoc\data\api\base\Component;
