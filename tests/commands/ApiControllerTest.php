8a3b54e2baca3a26915cc5cdf4e33eef492bc857
Maksim Spirkov <63721828+mspirkov@users.noreply.github.com>
2025-11-26 00:10:50 +0300

    Fix #337: Log invalid tags

--- tests/commands/ApiControllerTest.php
+++ tests/commands/ApiControllerTest.php
@@ -99,5 +99,15 @@ class ApiControllerTest extends TestCase
         $sourceFilesCount = count(FileHelper::findFiles($sourceFilesDir, ['recursive' => true]));
 
         $this->assertSame($sourceFilesCount, $filesCount);
+
+        $warningsContent = file_get_contents("{$outputPath}/warnings.txt");
+        // Remove the dynamic parts of the file paths
+        $warningsContent = preg_replace('/(\s*\[file\] => ).*(\/tests\/.*\.php)/', '$1$2', $warningsContent);
+        $this->assertMatchesTextSnapshot($warningsContent);
+
+        $errorsContent = file_get_contents("{$outputPath}/errors.txt");
+        // Remove the dynamic parts of the file paths
+        $errorsContent = preg_replace('/(\s*\[file\] => ).*(\/tests\/.*\.php)/', '$1$2', $errorsContent);
+        $this->assertMatchesTextSnapshot($errorsContent);
     }
 }
