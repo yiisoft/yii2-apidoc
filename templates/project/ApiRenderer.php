b9c8a3789e5cc7c26a66dfb980ff3c211cd2161c
Maksim Spirkov <63721828+mspirkov@users.noreply.github.com>
2025-12-29 15:19:02 +0300

    Update ApiRenderer.php

--- templates/project/ApiRenderer.php
+++ templates/project/ApiRenderer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
@@ -8,9 +9,6 @@
 namespace yii\apidoc\templates\project;
 
 use Yii;
-use yii\apidoc\helpers\ApiIndexer;
-use yii\helpers\Console;
-use yii\helpers\FileHelper;
 
 /**
  *
@@ -22,7 +20,7 @@ class ApiRenderer extends \yii\apidoc\templates\bootstrap\ApiRenderer
     use \yii\apidoc\templates\bootstrap\RendererTrait;
 
     public $layout = '@yii/apidoc/templates/bootstrap/layouts/api.php';
-	public $indexView = '@yii/apidoc/templates/bootstrap/views/index.php';
+    public $indexView = '@yii/apidoc/templates/bootstrap/views/index.php';
 
     /**
      * @inheritdoc
@@ -35,19 +33,19 @@ class ApiRenderer extends \yii\apidoc\templates\bootstrap\ApiRenderer
         if ($this->controller !== null) {
             $this->controller->stdout('generating extension index files...');
         }
-		
-		$types = array_merge($context->classes, $context->interfaces, $context->traits);
 
-		$appTypes = $this->filterTypes($types, 'app');
+        $types = array_merge($context->classes, $context->interfaces, $context->traits);
 
-		// It's a hack, but we'll go with it for now.
-		$readme = @file_get_contents($this->readmeUrl);
-		$indexFileContent = $this->renderWithLayout($this->indexView, [
-			'docContext' => $context,
-			'types' => $appTypes ?: null,
-			'readme' => $readme ?: null,
-		]);
-	
-		file_put_contents($targetDir . '/index.html', $indexFileContent);
+        $appTypes = $this->filterTypes($types, 'app');
+
+        // It's a hack, but we'll go with it for now.
+        $readme = @file_get_contents($this->readmeUrl);
+        $indexFileContent = $this->renderWithLayout($this->indexView, [
+            'docContext' => $context,
+            'types' => $appTypes ?: null,
+            'readme' => $readme ?: null,
+        ]);
+
+        file_put_contents($targetDir . '/index.html', $indexFileContent);
     }
 }
