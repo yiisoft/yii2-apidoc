a181c07cefe9925611d2f99b5951a78ccce301be
Максим Спирков <spirkov.2001@mail.ru>
2026-02-17 12:34:16 +0400

    Add tests

--- tests/data/api/animal/Animal.php
+++ tests/data/api/animal/Animal.php
@@ -224,4 +224,14 @@ abstract class Animal extends BaseObject
     {
         return $this;
     }
+
+    /**
+     * Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     *
+     * Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when
+     * an unknown printer took a galley of type and scrambled it to make a type specimen book.
+     */
+    public function methodWithShortAndDetailedDescriptions(): void
+    {
+    }
 }
